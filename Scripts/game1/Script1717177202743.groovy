d5ecf40df7b49bb051d165a32fd783a7b9482595
rasaarah <ra.sarahkamilah@gmail.com>
2024-06-01 01:00:55 +0700

    done

---
+++ Scripts/game1/Script1717177202743.groovy
@@ -0,0 +1,42 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.TestObjectProperty
+
+
+WebUI.openBrowser("https://zzzscore.com/1to50/en/")
+
+WebUI.waitForElementPresent(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//div[@id='grid']"), 0)
+for (int number = 1; number <= 50; number++) {
+	WebUI.click(new TestObject().addProperty('xpath', ConditionType.EQUALS, "//div[text()='" + number + "']"))
+}
+
+//WebUI.closeBrowser()
+
+
+
+
+
+
+
+
+
